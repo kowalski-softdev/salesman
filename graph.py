eae9f18677474d729759179f6f86a1a104653c75
kowalski-softdev <kowalski.softdev@gmail.com>
2024-02-24 11:42:53 +0100

    fixed bugs in is_path_traversable method: - no longer add element to the _adjacency_dict during checking path longer than 1 - properly checks paths of length <2 added functionality to add_edge method: - during adding edge to a new vertex, the vertex is added to _adjacency_dict without outgoing edges added comments

--- graph.py
+++ graph.py
@@ -10,12 +10,16 @@ class Graph:
         self._adjacency_dict[source].update({target: weight})
         if not self._directed:
             self._adjacency_dict[target].update({source: weight})
+        else:
+            if self._adjacency_dict.get(target) is None:
+                self._adjacency_dict[target]
 
     def calculate_cost(self, path: list) -> int|float:
         i = 0
         cost = 0
         while i<len(path)-1:
             try:
+                #possible bug - adding element to dict
                 cost += self._adjacency_dict[path[i]][path[i+1]]
             except KeyError as e:
                 raise KeyError(f"The path cannot be traveled from {path[i]} to {path[i+1]}")
@@ -24,21 +28,34 @@ class Graph:
 
     #does the path can be traversed
     def is_path_traversable(self, path: list) -> bool:
+        if len(path) == 0:
+            raise ValueError('The path is empty.')
+
+        if len(path) == 1:
+            if path[0] in self.vertices_list:
+                return True
+            else:
+                return False
+
         i = 0
         while i < len(path)-1:
             try:
-                self._adjacency_dict[path[i]][path[i+1]]
-            except KeyError:
+                condition = self._adjacency_dict.get(path[i]).get(path[i+1])
+            except (KeyError, AttributeError):
+                return False
+            if condition is None:
                 return False
             i += 1
         return True
 
     @property
     def vertices_count(self):
+        """The number of vertices in the graph property."""
         return len(self._adjacency_dict)
 
     @property
     def vertices_list(self):
+        """The list of names for vertices property."""
         return list(self._adjacency_dict)
 
 if __name__ == "__main__":
