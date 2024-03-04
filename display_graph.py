6e9501fbbc82b5187da755cd11aec82c75e5e721
kowalski-softdev <kowalski.softdev@gmail.com>
2024-03-04 22:11:44 +0100

    Class for rendering a graph.

--- display_graph.py
+++ display_graph.py
@@ -1,18 +1,28 @@
-import pyglet
+from pyglet.window import Window
+from pyglet.app import run
+from pyglet.shapes import Circle, Line
+from pyglet.graphics import Batch
 
-window = pyglet.window.Window(width=1280, height=720, caption="Graph")
-window.set_location(x=400, y=200)
-star = pyglet.shapes.Star(100,100,7,3,5, color=(50,225,30))
-circle = pyglet.shapes.Circle(x=700, y=150, radius=10, color=(50, 225, 30))
+class Renderer(Window):
+    def __init__(self, x_window_size, y_window_size, adj_dict):
+        self.x_window_size = x_window_size
+        self.y_window_size = y_window_size
+        super().__init__(self.x_window_size, self.y_window_size, "Graph")
+        self.batch = Batch()
+        nodes_arr = []
+        for node_pos in adj_dict:
+            nodes_arr.append(Circle(node_pos[0], node_pos[1], radius=50, color=(255,255,0), batch=self.batch))
 
-@window.event
-def on_draw() -> None:
-    window.clear()
-    circle.draw()
-    star.draw()
+    def on_draw(self):
+        self.clear()
 
-def display():
-    pyglet.app.run()
 
 if __name__ == "__main__":
-    display()
+    #x = int(input('Size of x? '))
+    #y = int(input('Size of y? '))
+    x = 1280
+    y = 640
+    adj_dict = {(10,20): {(20,10): 5}, (20,10): {(10,20): 5}}
+
+    renderer = Renderer(x, y, adj_dict)
+    run()
